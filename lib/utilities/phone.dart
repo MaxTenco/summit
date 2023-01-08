384988150fec4aff142a98d10157e3e124051808
Marco Coppola <ingcoppolamarco@gmail.com>
2023-01-08 17:53:42 +0100

    Add make WhatsApp Chat

--- lib/utilities/phone.dart
+++ lib/utilities/phone.dart
@@ -1,11 +1,32 @@
+import 'dart:io';
+
 import 'package:url_launcher/url_launcher.dart';
 
-class SummitPhone {
+abstract class SummitPhone {
   static Future<void> makePhoneCall(String phoneNumber) async {
     final Uri launchUri = Uri(
       scheme: 'tel',
       path: phoneNumber,
     );
     await launchUrl(launchUri);
+  }
+
+  static Future<void> makeWhatsAppChat(String phoneNumber, String message) async {
+    late final String url;
+
+    if (Platform.isAndroid) {
+      url = 'https://wa.me/$phoneNumber/?text=${Uri.parse(message)}';
+    } else {
+      url = 'https://api.whatsapp.com/send?phone=$phoneNumber=${Uri.parse(message)}';
+    }
+
+    final Uri uri = Uri.parse(url);
+
+    if (!await launchUrl(
+      uri,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw 'Could not launch $uri';
+    }
   }
 }
