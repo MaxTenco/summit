2a544f3b850cc2a3bd236800433e27aa7c181a13
Marco Coppola <ingcoppolamarco@gmail.com>
2023-04-26 12:17:37 +0200

    Add notification bell component

--- lib/summit.dart
+++ lib/summit.dart
@@ -1,6 +1,7 @@
 library summit;
 
 export 'components/actions/circle_button.dart';
+export 'components/notification_bell.dart';
 export 'components/section.dart';
 export 'splash_screen_boot/splash_screen_boot.dart';
 export 'utilities/phone.dart';
