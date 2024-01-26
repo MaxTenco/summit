c819b9fbce0cb4a4336febbcc640a9d038d9fd82
Marco Coppola <ingcoppolamarco@gmail.com>
2024-01-26 16:55:32 +0100

    Add export of gap utility

--- lib/summit.dart
+++ lib/summit.dart
@@ -4,6 +4,7 @@ export 'components/actions/circle_button.dart';
 export 'components/notification_bell.dart';
 export 'components/section.dart';
 export 'splash_screen_boot/splash_screen_boot.dart';
+export 'utilities/gap.dart';
 export 'utilities/phone.dart';
 
 /// A Calculator.
