712193526181b7db6aa80f2bb8362ef5a92c8dab
Marco Coppola <ingcoppolamarco@gmail.com>
2024-04-03 23:51:40 +0200

    Add extension of CheckFileExtension

--- lib/utilities/string_helpers.dart
+++ lib/utilities/string_helpers.dart
@@ -1,3 +1,7 @@
 extension ConvertNullableStringoNullableIntExtension on String? {
   int? toNullableInt() => this == null ? null : int.parse(this!);
 }
+
+extension CheckFileExtensionExtension on String {
+  bool get isSvg => substring(lastIndexOf('.')) == '.svg';
+}
