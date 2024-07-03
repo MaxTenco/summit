dc7298bd38bb42c8f4682f89db4117befce12ffc
Marco Coppola <ingcoppolamarco@gmail.com>
2024-07-03 23:05:53 +0200

    Update pubspec.yaml

--- lib/summit.dart
+++ lib/summit.dart
@@ -1,5 +1,7 @@
 library summit;
 
+export 'package:url_launcher/url_launcher.dart';
+
 export 'splash_screen_boot/splash_screen_boot.dart';
 export 'src/components/components.dart';
 export 'utilities/gap.dart';
